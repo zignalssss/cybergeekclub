e540ae67e2e8497b950ad9c5174db3b8c5f51a28
zjeee <wiritipon.t@ku.th>
2024-05-09 21:30:29 +0700

    add hamburger menu responsive

--- src/components/navbar/navbar.tsx
+++ src/components/navbar/navbar.tsx
@@ -16,10 +16,15 @@ import { FaFacebookSquare, FaCalendarAlt, FaInstagram } from "react-icons/fa";
 import { SiGmail } from "react-icons/si";
 import { MdAnnouncement } from "react-icons/md";
 import { IoMenu } from "react-icons/io5";
+import { IoMdClose } from "react-icons/io";
 
-const variants = {
+const variantsleft = {
 	open: { opacity: 1, x: 0 },
 	closed: { opacity: 0, x: "-100%" },
+}
+const variantsright = {
+	open: { opacity: 1, x: 0 },
+	closed: { opacity: 0, x: "100%" },
 }
 
 const navbar = () => {
@@ -28,13 +33,14 @@ const navbar = () => {
 	const [activeC, setActiveC] = useState<string | null>(null); //for contact
 	const [isOpen, setIsOpen] = useState(true)//nav mobile
 	const [isDrop, setIsDrop] = useState(false)
+	const [isLogin,setIsLogin] = useState(true)
 	return (
 		<>
 			<nav className="bg-opacity-60 shadow-5xl backdrop-filter backdrop-blur-sm sticky top-0 bg-black shadow shadow-white/[0.2] z-50 w-100 px-8 md:px-auto">
 				<div className="h-16 md:h-16 mx-auto md:px-4 flex  items-center justify-between flex-wrap md:flex-nowrap">
 
 					<Link href="/">
-						<div className="mt-1 md:mt-0 text-white font-semibold font-kanit md:order-1 transition-all duration-250 hover:scale-110 ">
+						<div className="hidden lg:grid mt-1 md:mt-0 text-white font-semibold font-kanit order-2 md:order-1 transition-all duration-250 hover:scale-110 ">
 							<div className='text-xl px-5 md:text-2xl md:px-4'>
 								CGC
 							</div>
@@ -139,15 +145,61 @@ const navbar = () => {
 							</button>
 						</Link>
 					</div>
-					<div className='lg:hidden md:order-3'>
-						<button className='btn text-white' onClick={() => { setIsOpen(!isOpen) }}>
-							This is But
-						</button>
+					{/* <div className='lg:hidden grid grid-cols-3 gap-11'>
+						<div className='btn ' onClick={() => { setIsOpen(!isOpen) }}>
+							teet
+						</div>
+						<div className='font-kanit font-semibold r'>
+							<div className='text-xl px-5 md:text-2xl md:px-4'>
+								CGC
+							</div>
+							<div className='text-xs md:text-xs'>
+								CyberGeekClub
+							</div>
+						</div>
+						<div className="avatar">
+							<div className="w-12 scale-75 rounded-full ring ring-primary ring-offset-base-100 ring-offset-2">
+								<img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
+							</div>
+						</div>
+					</div> */}
+					<div className="navbar ">
+						<div className="navbar-start">
+							<div className="dropdown">
+								<button onClick={() => { setIsOpen(!isOpen) }}>
+									{isOpen?< IoMdClose className='mt-3 text-2xl md:text-4xl transition-all duration-250'/> : <IoMenu className='mt-3 text-2xl md:text-4xl transition-all duration-250'/>}
+									
+								</button>
+							</div>
+						</div>
+						<div className="navbar-center">
+							<div className='font-kanit font-semibold r'>
+								<div className='text-xl px-5 md:text-2xl md:px-4'>
+									CGC
+								</div>
+								<div className='text-xs md:text-xs'>
+									CyberGeekClub
+								</div>
+							</div>
+						</div>
+						<div className="navbar-end">
+							<button className="btn btn-ghost btn-circle">
+								<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M21 21l-6-6m2-5a7 7 0 11-14 0 7 7 0 0114 0z" /></svg>
+							</button>
+							<button className="btn btn-ghost btn-circle">
+								<div className="indicator">
+									<svg xmlns="http://www.w3.org/2000/svg" className="h-5 w-5" fill="none" viewBox="0 0 24 24" stroke="currentColor"><path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M15 17h5l-1.405-1.405A2.032 2.032 0 0118 14.158V11a6.002 6.002 0 00-4-5.659V5a2 2 0 10-4 0v.341C7.67 6.165 6 8.388 6 11v3.159c0 .538-.214 1.055-.595 1.436L4 17h5m6 0v1a3 3 0 11-6 0v-1m6 0H9" /></svg>
+									<span className="badge badge-xs badge-primary indicator-item"></span>
+								</div>
+							</button>
+						</div>
 					</div>
 				</div>
+				{/* Responsive Navbar  Design*/}
+
 				<motion.div
 					animate={isOpen ? "open" : "closed"}
-					variants={variants}
+					variants={variantsleft}
 					transition={{ type: "spring", duration: 0.5 }}
 					className='flex justify-end relative z-100'
 				>
@@ -155,15 +207,7 @@ const navbar = () => {
 						className={`z-0 top-0 flex-1 font-kanit justify-self-center pb-3  lg:pb-0 lg:mt-0 lg:hidden ${isOpen ? 'p-12 lg:p-0 block' : 'hidden'
 							}`}
 					>
-						<div className='flex gap-4'>
-							<div className="avatar">
-								<div className="w-10 rounded-full">
-									<img src="https://img.daisyui.com/images/stock/photo-1534528741775-53994a69daeb.jpg" />
-								</div>
-							</div>
-							<div className='text-sm mt-2'> Name UsurName</div>
-						</div>
-						<ul className="h-screen w-52 lg:h-auto items-center justify-center lg:flex ">
+						<ul className="h-screen max-w-screen lg:h-auto items-center justify-center lg:flex ">
 							<li className="pb-6 text-base text-white py-2 lg:px-6 text-center border-b-2 lg:border-b-0  hover:text-green-400  border-green-400  lg:hover:bg-transparent">
 								<Link href="/" onClick={() => setIsOpen(!isOpen)}>
 									หน้าเเรก
@@ -175,7 +219,7 @@ const navbar = () => {
 								</Link>
 							</li>
 							<li className=" pb-6 text-base text-white py-2 px-6 text-center  border-b-2 lg:border-b-0    border-green-400  lg:hover:bg-transparent">
-							
+
 								<details>
 									<summary>เกี่ยวกับเรา</summary>
 									<ul className='flex flex-col gap-2 ml-3 mt-4'>
@@ -208,7 +252,7 @@ const navbar = () => {
 								</details>
 							</li>
 							<li className="pb-6 text-base text-white py-2 px-6 text-center  border-b-2 lg:border-b-0    border-green-400   md:hover:bg-transparent">
-							
+
 								<details>
 									<summary>ข่าวสาร</summary>
 									<ul className='flex flex-col gap-2 ml-3 mt-4'>
