87fdb13e36cbee7c52af5c14b07050595bfb8ef2
zjeee <wiritipon.t@ku.th>
2024-05-10 14:41:57 +0700

    add scroll activities in mainpage

--- src/components/mainpage_comp/yorhistory.tsx
+++ src/components/mainpage_comp/yorhistory.tsx
@@ -1,13 +1,15 @@
 'use client';
 import React from 'react'
 
+
 const yorhistory = () => {
   return (
     <div className='flex justify-center h-screen font-kanit'>
-        <div className='mt-20'>
-          <h1 className=' ml-5 md:ml-[20%] lg:ml-[25%] xl:ml-[30%]  text-sm md:text-2xl font-semibold mb-10'><span className=' drop-shadow-[0_0_3px_rgba(22,101,52)] text-green-500 underline underline-offset-8 mr-3'>C y b e r G e e k C l u b</span> เป็นชมรมเกี่ยวกับอะไร?</h1>
-          <p className='max-w-80 md:max-w-2xl lg:max-w-4xl xl:lg:max-w-6xl text-sm md:text-xl text-neutral-300 indent-10'>เราเป็นชมรมที่เกี่ยวกับ Technology CyberSecurity เเละการ Coding เราเชื่อว่าเทคโนโลยีและการโค้ดมี perpotential ที่ยิ่งใหญ่ในการประเมินและแก้ไขปัญหาที่ท้าทายในโลกปัจจุบัน ไม่ว่าจะเป็นการพัฒนาเว็บไซต์ การสร้างแอพพลิเคชั่น หรือ การเขียนโค้ดที่ช่วยในการ Analyze data เราจึงมีความกระตือรือร้นที่จะเรียนรู้และแบ่งปันความรู้เกี่ยวกับ Technology เเละ การCoding</p>
+        <div className='flex flex-col justify-center'>
+          <h1 className=' ml-[10%] md:ml-[20%] lg:ml-[25%] xl:ml-[30%]  text-sm md:text-2xl font-semibold mb-10'><span className=' drop-shadow-[0_0_3px_rgba(22,101,52)] text-green-500 underline underline-offset-8 mr-3'>C y b e r G e e k C l u b</span> เป็นชมรมเกี่ยวกับอะไร?</h1>
+          <p className='ml-[10%] md:ml=0 max-w-80 md:max-w-2xl lg:max-w-4xl xl:lg:max-w-6xl text-sm md:text-xl text-neutral-300 indent-10'>เราเป็นชมรมที่เกี่ยวกับ Technology CyberSecurity เเละการ Coding เราเชื่อว่าเทคโนโลยีและการโค้ดมี perpotential ที่ยิ่งใหญ่ในการประเมินและแก้ไขปัญหาที่ท้าทายในโลกปัจจุบัน ไม่ว่าจะเป็นการพัฒนาเว็บไซต์ การสร้างแอพพลิเคชั่น หรือ การเขียนโค้ดที่ช่วยในการ Analyze data เราจึงมีความกระตือรือร้นที่จะเรียนรู้และแบ่งปันความรู้เกี่ยวกับ Technology เเละ การCoding</p>        
         </div>
+      
     </div>
   )
 }
