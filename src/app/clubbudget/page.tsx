0a1263805e866e7ac7b5d96aa449c23132478d5a
Zhilshi04 <pannet.v@ku.th>
2024-05-11 19:38:58 +0700

    update componet

--- src/app/clubbudget/page.tsx
+++ src/app/clubbudget/page.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import All_Budget from "@/components/badget/all-budget";
 import Simple_Detial_Budget from "@/components/badget/simple_detial_budget";
+import All_Detial_Budget from "@/components/badget/all-detial-budget";
 export default function Budget(){
     return(
         <div className="flex flex-col items-center h-dvh bg-black overflow-auto scroll-smooth scrollbar-hide">
@@ -16,48 +17,9 @@ export default function Budget(){
                     <Simple_Detial_Budget typeBudget="รายจ่าย" income={0} budgetSupport={0}/>
                 </div>
                 <div className="row-span-2 flex justify-around">
-                    <div className="bg-[#1E1E1E] rounded-2xl h-full w-2/4 my-4 mx-4 flex flex-col items-center">
-                        <h1 className="px-4 py-4 font-kanit font-bold text-2xl">รายระเอียดงบประมาณ</h1>
-                        <div className="flex justify-around w-full h-full overflow-auto scroll-smooth scrollbar-hide">
-                            <div className="px-10 py-2">
-                                <h2 className="px-5 pt-4 font-kanit font-bold text-l">รายได้จากเงินรายได้</h2>
-                                <div className=" bg-[#302E2E] w-full rounded-lg px-2 py-2 my-2">
-                                    <h1>งบสนับสนุน</h1>
-                                    <h1 className="text-green-500">+ 100.00 บาท</h1>
-                                </div>
-                        
-                                
-                            </div>
-                            <div className="px-5 py-2 py-2 ">
-                                <h2 className="px-10 pt-4 font-kanit font-bold text-l ">รายได้จากงบอุดหนุน</h2>
-                                <div className=" bg-[#302E2E] w-full rounded-lg px-2 py-2 my-2">
-                                    <h1>งบสนับสนุน</h1>
-                                    <h1 className="text-green-500">+ 100.00 บาท</h1>
-                                </div>
-                          
-                            </div>
-                        </div>
-                    </div>
-                    <div className="bg-[#1E1E1E] rounded-2xl h-full w-2/4 my-4 mx-4 flex flex-col items-center">
-                        <h1 className="px-4 py-4 font-kanit font-bold text-2xl">รายระเอียดรายจ่าย</h1>
-                        <div className="flex justify-around w-full h-full overflow-auto scroll-smooth scrollbar-hide">
-                            <div className="px-10 py-2 ">
-                                <h2 className="px-5 pt-4 font-kanit font-bold text-l">รายจ่ายจากเงินรายจ่าย</h2>
-                                <div className=" bg-[#302E2E] w-full rounded-lg px-2 py-2 my-2">
-                                    <h1>งบสนับสนุน</h1>
-                                    <h1 className="text-red-500">- 100.00 บาท</h1>
-                                </div>
-                            </div>
-                            <div className="px-10 py-2">
-                                <h2 className="px-5 pt-4 font-kanit font-bold text-l">รายจ่ายจากงบอุดหนุน</h2>
-                                <div className=" bg-[#302E2E] w-full rounded-lg px-2 py-2 my-2">
-                                    <h1>งบสนับสนุน</h1>
-                                    <h1 className="text-red-500">- 100.00 บาท</h1>
-                                </div>
-                                
-                            </div>
-                        </div>
-                    </div>
+                {/* รายละเอียดรายรับรายจ่าย */}
+                    <All_Detial_Budget typeBudget="รายระเอียดงบประมาณ" detial_income={[{ key: "งบสนับสนุน", value: 1000 }]} detial_support={[{ key: "งบสนับสนุน", value: 1000 },]}/>
+                    <All_Detial_Budget typeBudget="รายระเอียดรายจ่าย" detial_income={[{ key: "งบสนับสนุน", value: 1000 }]} detial_support={[{ key: "งบสนับสนุน", value: 1000 },]}/>
                 </div>
             </div>
         </div>
