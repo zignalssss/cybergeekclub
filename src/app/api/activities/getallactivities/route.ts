599e506f7f24b7ea4eb5bdd3c74689298c6523ff
PerthTheIamCat <thongcum2546@gmail.com>
2024-06-10 21:32:33 +0700

    add api : get all activities

---
+++ src/app/api/activities/getallactivities/route.ts
@@ -0,0 +1,16 @@
+import { PrismaClient } from "@prisma/client";
+
+const prisma: PrismaClient = new PrismaClient();
+
+export async function GET(request: Request) {
+    try{
+        const activities = await prisma.corporate_activity.findMany({
+          orderBy: {
+            built : 'desc'
+          }
+        })
+        return Response.json({ message: "GET USER Success", data: activities }, { status: 200 });
+    }catch(error: unknown){
+      return Response.json({ message: "GET USER Unsuccess"}, { status: 500 });
+    }
+  }
