da71c9fad41cd5e93a0981eee613fd82abfb602c
zjeee <wiritipon.t@ku.th>
2024-06-16 01:55:43 +0700

    eventcalendar fix layout

--- src/app/eventcalendar/page.tsx
+++ src/app/eventcalendar/page.tsx
@@ -9,6 +9,16 @@ const Eventcalendar = () => {
         },
         {
             id:2,
+            title:"นิสิตใหม่ :ขึ้นทะเบียนเป็นนิสิตใหม่ออนไลน์ผ่านระบบ https://isea.ku.ac.th/STDWeb และลงทะเบียนยืนยันตัวบุคคลเพื่อใช้งานบัญชีผู้ใช้เครือข่ายนนทรี (Nontri Account) ของมหาวิทยาลัยเกษตรศาสตร์ ผ่านระบบhttps://accounts.ku.ac.th",
+            date:"(Tcas 1-4) ศ.7 - ศ.14 มิ.ย.67"
+        },
+        {
+            id:3,
+            title:"นิสิตใหม่ : ใช้บัญชีผู้ใช้เครือข่ายนนทรี (Nontri Account) ที่ได้รับดำเนินการ Login เข้าระบบลงทะเบียนเรียนนิสิต my.ku.th เพื่อชำระค่าธรรมเนียมการศึกษาผ่าน Mobile Banking และ เข้าระบบสารสนเทศนิสิต www.reg.src.ku.ac.th สำหรับการตรวจสอบข้อมูลทา",
+            date:"(Tcas 1-4) ศ.7 - ศ.14 มิ.ย.67"
+        },
+        {
+            id:4,
             title:"ปฏิทินการศึกษา TEST TEST",
             date:"(Tcas 1-4) ศ.7 - ศ.14 มิ.ย.67"
         }
@@ -18,14 +28,18 @@ const Eventcalendar = () => {
       
         <div className=''>
             <div className='text-center m-10 text-3xl'>ปฏิทินกิจกรรม</div>
-           {testlist.map((list,index) =>(
-                 <div className='p-4 grid grid-cols-3 gap-96 border border-white/15 mx-5'>
-                    <div>{list.id}</div>
-                    <div>{list.title}</div>
-                    <div>{list.date}</div>
-                 </div>
-           ))}
-            
+            <div className="p-4 grid grid-cols-3 gap-5 mx-10 font-semibold">
+                <div className="flex items-center">ID</div>
+                <div className="flex items-center">Title</div>
+                <div className="flex items-center justify-end">Date</div>
+            </div>
+            {testlist.map((list, index) => (
+            <div className="p-4 grid grid-cols-3 gap-5 border border-white/15 mx-10">
+                <div className="flex items-center">{list.id}</div>
+                <div className="flex items-center">{list.title}</div>
+                <div className="flex items-center justify-end">{list.date}</div>
+            </div>
+            ))}
         </div>
     </div>
   )
