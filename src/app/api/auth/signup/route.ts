e29d139cf617e68e814dff13e6ff7deda35bac60
PerthTheIamCat <thongcum2546@gmail.com>
2024-06-17 17:24:13 +0700

    update api signup

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -1,4 +1,4 @@
-import { PrismaClient } from "@prisma/client";
+import { account_code, PrismaClient } from "@prisma/client";
 import { v4 as uuidv4 } from 'uuid';
 import bcrypt from "bcrypt";
 import { NextResponse } from 'next/server';
@@ -44,42 +44,39 @@ export async function POST(req: Request): Promise<Response> {
             faculty_EN: string;
             major_TH: string;
             major_EN: string;
-            tag: string;
+            tag: account_code;
         };
- 
-
+        const currentDate = new Date();
+        const futureDate = new Date(currentDate.getTime() + 7 * 24 * 60 * 60 * 1000);
         await prisma.account.create({
             data: {
-                id: uuidv4(),
-                email,
-                password: bcrypt.hashSync(password, 10),
-                prefix_th: prefix_TH,
-                prefix_en: prefix_EN,
-                first_name_th: name_TH,
-                first_name_en: name_EN,
-                last_name_th: surname_TH,
-                last_name_en: surname_EN,
-                nickname_th: nickname_TH,
-                nickname_en: nickname_EN,
-                phone_number,
-                birthdate: new Date(birth_date),
-                student_id,
-                faculty_th: faculty_TH,
-                faculty_en: faculty_EN,
-                major_th: major_TH,
-                major_en: major_EN,
-                profile_image: null,
-                middle_name_th: null,
-                middle_name_en: null,
-                code: tag,
-                role: "MEMBER",
-                point: 20,
-                document: null,
-                status: "ORDINARY",
-                display_name: nickname_EN,
-                remaining_time: null,
-                wrong_password: 0,
-                ignored_account: null
+            id: uuidv4(),
+            email,
+            password: bcrypt.hashSync(password, 10),
+            prefix_th: prefix_TH,
+            prefix_en: prefix_EN,
+            first_name_th: name_TH,
+            first_name_en: name_EN,
+            last_name_th: surname_TH,
+            last_name_en: surname_EN,
+            nickname_th: nickname_TH,
+            nickname_en: nickname_EN,
+            phone_number,
+            birthdate: new Date(birth_date),
+            student_id,
+            faculty_th: faculty_TH,
+            faculty_en: faculty_EN,
+            major_th: major_TH,
+            major_en: major_EN,
+            profile_image: "",
+            middle_name_th: "",
+            middle_name_en: "",
+            code: tag,
+            role: "MEMBER",
+            point: 20,
+            display_name: nickname_EN,
+            remaining_time: futureDate,
+            banned: false,
             },
         });
 
